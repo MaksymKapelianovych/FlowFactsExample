060ede50d2217affb44ef5ec84416a01dfe9de84
MaksymKapelianovych <m.kapelianovych@gmail.com>
2024-05-05 20:33:40 +0300

    Initial commit

---
+++ Source/FlowFactsExample/FlowFactsExampleGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FlowFactsExampleGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AFlowFactsExampleGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AFlowFactsExampleGameMode();
+};
+
+
+
